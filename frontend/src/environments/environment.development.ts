26b3a22d5be4e690cc6277f69a4fd65effa9aa8e
Phaiboon Withanthamrong <touchfn@gmail.com>
2024-07-24 22:16:50 +0700

    Set up environment

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,6 +1,4 @@
 export const environment = {
-  // API_URL: 'http://192.168.1.37:3000/',
-  // IMAGE_URL: 'http://192.168.1.37:3000/images/',
   API_URL: 'http://localhost:3000/',
   IMAGE_URL: 'http://localhost:3000/images/',
 };
